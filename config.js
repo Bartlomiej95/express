d8ab4fe6f34dfc185befb76b14aa8168e30d8d02
Bartlomiej95 <panfilbartlomiej@gmail.com>
2020-10-17 15:53:34 +0200

    admin login, admin news

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    db: 'mongodb+srv://admin:nJD58rzEj1cZaWnt@cluster0.f06yx.mongodb.net/<dbname>?retryWrites=true&w=majority',
+    keySession: ['TWOJKLUCZ'],
+    maxAgeSession: 24 * 60 * 60 * 1000
+}
--- routes/admin.js
+++ routes/admin.js
@@ -1,10 +1,55 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const News = require('../models/news');
+
+const router = express.Router();
+
+// funkcja odpala się dla każdego requesta dla admina
+// wszystko co się znajduje w /admin będzie chronione sesją
+router.all('*', (req, res, next) => {
+    if(!req.session.admin){
+        res.redirect('/login');
+        return;
+    }
+
+    next();
+});
 
 /* GET home page. */
 router.get('/', (req, res) => {
-  res.render('admin', { title: 'Admin' });
+  News.find({}, (err, data) =>{
+    res.render('admin/index', { title: 'Admin', data });
+    
+  }); //pobieramy wszysttkie artykuły które sa w naszej bazie
+
 });
+
+router.get('/news/add', (req, res) => {
+  res.render('admin/news-form', { title: 'Dodaj news', body: {}, errors: {} });
+
+});
+
+router.post('/news/add', (req, res) => {
+  const body = req.body;
+
+  const newsData = new News(body);
+  const errors = newsData.validateSync();
+  
+  newsData.save((err) => {
+    if(err){
+      res.render('admin/news-form', { title: 'Dodaj news', errors, body });
+      return;
+    }  
+    res.redirect('/admin');
+  });
+});
+
+router.get('/news/delete/:id', (req, res) => {
+  News.findByIdAndDelete(req.params.id, (err) => {
+    res.redirect('/admin')
+  })
+
+});
+
 
 
 
