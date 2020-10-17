211bf17121c858f7ca71b48f53dc9f0bae7bdf59
Bartlomiej95 <panfilbartlomiej@gmail.com>
2020-10-17 18:34:14 +0200

    news filter front

--- routes/news.js
+++ routes/news.js
@@ -1,13 +1,20 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const News = require('../models/news');
 
 /* GET home page. */
 router.get('/', (req, res) => {
-  res.render('news', { title: 'News' });
+  const search = req.query.search || '';
+
+  const findNews = News
+    .find({ title: new RegExp(search.trim(), 'i') })
+    .sort({ created: -1});
+
+  findNews.exec((err, data) => {
+    res.render('news', { title: 'News', data, search }); //przekazujemy dane do widoku
+  })
+
 });
-
-
-
 
 
 module.exports = router;
