fdcb0e8436f90a383fc63918d192956dee035d62
Bartlomiej95 <panfilbartlomiej@gmail.com>
2020-10-17 21:47:33 +0200

    quiz form

--- routes/quiz.js
+++ routes/quiz.js
@@ -1,11 +1,36 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const Quiz = require('../models/quiz')
+const router = express.Router();
 
 /* GET home page. */
 router.get('/', (req, res) => {
-  res.render('quiz', { title: 'Quiz' });
+  const show = !req.session.vote;
+
+  Quiz.find({}, (err, data) => {
+    let sum = 0;
+    data.forEach((item) => {
+      sum += item.vote;
+    })
+
+    res.render('quiz', { title: 'Quiz', data, show, sum });
+
+  })
+
 });
 
+router.post('/', (req, res) => {
+  const id = req.body.quiz;
+ 
+  Quiz.findOne({ _id: id}, (err, data) => {
+    data.vote = data.vote + 1;
+    data.save((err) =>{
+      req.session.vote = 1;
+      res.redirect('/quiz');
+    });
+
+  })
+
+});
 
 
 
